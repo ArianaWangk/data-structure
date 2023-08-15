a12c222be5014b1533df5a8571ae19ee42d1822d
Ariana Wang <140805359+ArianaWangk@users.noreply.github.com>
2023-08-15 14:46:23 +0800

    Update Min-heap or priority queue(leetcode 703).py

--- Min-heap or priority queue(leetcode 703).py
+++ Min-heap or priority queue(leetcode 703).py
@@ -2,7 +2,8 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         # minHeap w/ K largest integers
         self.minHeap, self.k = nums, k
-        heapq.heapify(self.minHeap)
+        heapq.heapify(self.minHeap) 
+        # heapify−将普通列表转换为堆。在结果堆中，最小的元素被推到索引位置0。但是其余的数据元素没有必要排序。heappush−该函数在不改变当前堆的情况下向堆中添加元素。
         while len(self.minHeap) > k:
             heapq.heappop(self.minHeap)
 
